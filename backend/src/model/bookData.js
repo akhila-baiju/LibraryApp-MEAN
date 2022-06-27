89688256eb9c16b62437b2cea7aecd31e132bccf
akhila-baiju <101245217+akhila-baiju@users.noreply.github.com>
2022-06-27 15:08:59 +0530

    Update bookData.js

--- backend/src/model/bookData.js
+++ backend/src/model/bookData.js
@@ -1,6 +1,7 @@
 
 const mongoose =require('mongoose');
-mongoose.connect("mongodb://localhost:27017/LibraryApp");
+//mongoose.connect("mongodb://localhost:27017/LibraryApp");
+mongoose.connect("mongodb+srv://akhila:akhila123@cluster0.upuqqlq.mongodb.net/LibraryApp");
 const Schema = mongoose.Schema;
 
 var BookSchema = new Schema({
